4074b4997ccc6de7a5c99969eafd3ddb7d6065d0
Axel CHARLASSIER <acharlas@e1r8p4.42.fr>
2019-11-22 17:15:53 +0100

    undefined behaviour ma gueule

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acharlas <acharlas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 17:02:26 by acharlas          #+#    #+#             */
-/*   Updated: 2019/11/22 16:39:10 by acharlas         ###   ########.fr       */
+/*   Updated: 2019/11/22 17:14:52 by acharlas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int	ray_intersect(const vect3f orig, const vect3f dir, float *t0, t_listobj *lis
 
 }
 
-int scene_intersect(vect3f orig, vect3f dir, t_listobj *listobj)
+int scene_intersect(vect3f orig, vect3f dir, t_listobj *listobj, vect3f *color)
 {
 	float spheres_dist = FLT_MAX;
 	while(listobj)
@@ -75,6 +75,7 @@ int scene_intersect(vect3f orig, vect3f dir, t_listobj *listobj)
 		float dist_i;
 		if(ray_intersect(orig, dir, &dist_i, listobj) && dist_i < spheres_dist)
 		{
+				*color = ((t_sphere *)(listobj->data))->color;
 				spheres_dist = dist_i;
 		}
 		listobj = listobj->next;
@@ -82,15 +83,15 @@ int scene_intersect(vect3f orig, vect3f dir, t_listobj *listobj)
 	return (spheres_dist < 1000);
 }
 
-
 vect3f cast_ray(vect3f orig, vect3f dir, t_listobj *listobj)
 {
 		float sphere_dist = FLT_MAX;
-		if (!scene_intersect(orig, dir, listobj))
+		vect3f color;
+		if (!scene_intersect(orig, dir, listobj, &color))
 		{
-			return (ft_fill(0.60, 0.99, 0.99));
+			return (ft_fill(0.57, 0, 0.25));
 		}
-		return (((t_sphere *)(listobj->data))->color);
+		return (color);
 }
 
 void render(t_listobj *listobj, const int width, const int height, void *mlx, void *mlx_window)
@@ -146,22 +147,28 @@ void ft_lstadd_front(t_listobj **alst, t_listobj *new)
 int main(void)
 {
 	const int width = 1000;
-	const int height = 600;
+	const int height = 1000;
 	t_listobj *listobj = NULL;
 	
 	t_sphere *sphere;
 	t_sphere *sphere2;
+	t_sphere *sphere3;
 	sphere = malloc(sizeof(t_sphere));
-	sphere->pos = ft_fill(20, 20, -30);
+	sphere->pos = ft_fill(0, 0, -15);
 	sphere->r = 2;
-	sphere->color = ft_fill(1, 0, 0);
+	sphere->color = ft_fill(0.87, 0.70, 1);
 	
 	ft_lstadd_front(&listobj, ft_lstnew(sphere));
 	sphere2 = malloc(sizeof(t_sphere));
-	sphere2->pos = ft_fill(20, 20, -36);
+	sphere2->pos = ft_fill(10, 0, -25);
 	sphere2->r = 2;
-	sphere2->color = ft_fill(0.949, 0.541, 0.835);
+	sphere2->color = ft_fill(0.486, 0.27, 0.65);
 	ft_lstadd_front(&listobj, ft_lstnew(sphere2));
+	sphere3 = malloc(sizeof(t_sphere));
+	sphere3->pos = ft_fill(20, 0, -35);
+	sphere3->r = 2;
+	sphere3->color = ft_fill(0.27, 0.08, 0.431);
+	ft_lstadd_front(&listobj, ft_lstnew(sphere));
 	void *mlx = mlx_init();
 	void *mlx_window = mlx_new_window(mlx, width, height, "Image");
 	render(listobj, width, height, mlx, mlx_window);
